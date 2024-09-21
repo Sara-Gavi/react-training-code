797405cdb6032e02b7f52fe982dac6df46c9f539
Sara-Gavi <sara.gavilan.castro@gmail.com>
2024-09-21 20:41:58 +0200

    Create Form component and improve CSS styling

--- flower-gallery/src/components/App.jsx
+++ flower-gallery/src/components/App.jsx
@@ -1,15 +1,13 @@
 import "../styles/App.css";
+import Header from "./Header";
+import Form from "./Form";
 
 function App() {
   return (
     <div className="app">
-      <header>
-        <h1>Flower gallery</h1>
-      </header>
+      <Header />
       <main>
-        <form>
-          <input type="text" placeholder="Find your flowers" value=""></input>
-        </form>
+        <Form />
         <ul className="image__gallery">
           <li className="image__card">
             <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.teleflora.es%2Fproducto%2Framo-rosas-rojas-xl-rosas-a-domicilio-34439&psig=AOvVaw0IReJu5Rm2qgHVvrdJfaHR&ust=1727024012632000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKCBs_y_1IgDFQAAAAAdAAAAABAE"></img>
---
+++ flower-gallery/src/components/Form.jsx
@@ -0,0 +1,8 @@
+function Form() {
+  return (
+    <form>
+      <input type="text" placeholder="Find your flowers" value=""></input>
+    </form>
+  );
+}
+export default Form;
