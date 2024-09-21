ccf878ab7b3a09f347a39681362e49d788b357a4
Sara-Gavi <sara.gavilan.castro@gmail.com>
2024-09-21 19:49:06 +0200

    Set up initial HTML structure, styles, and App component for the flower gallery

--- flower-gallery/src/App.jsx
+++ flower-gallery/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <header>
+        <h1>Flower gallery</h1>
+      </header>
+      <main>
+        <form>
+          <input type="text" placeholder="Find your flowers" value=""></input>
+        </form>
+        <ul className="image__gallery">
+          <li className="image__card">
+            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.teleflora.es%2Fproducto%2Framo-rosas-rojas-xl-rosas-a-domicilio-34439&psig=AOvVaw0IReJu5Rm2qgHVvrdJfaHR&ust=1727024012632000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKCBs_y_1IgDFQAAAAAdAAAAABAE"></img>
+            <h3>Red Flowers</h3>
+          </li>
+          <li className="image__card">
+            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.teleflora.es%2Fproducto%2Framo-rosas-rojas-xl-rosas-a-domicilio-34439&psig=AOvVaw0IReJu5Rm2qgHVvrdJfaHR&ust=1727024012632000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKCBs_y_1IgDFQAAAAAdAAAAABAE"></img>
+            <h3>white Flowers</h3>
+          </li>
+          <li className="image__card">
+            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.teleflora.es%2Fproducto%2Framo-rosas-rojas-xl-rosas-a-domicilio-34439&psig=AOvVaw0IReJu5Rm2qgHVvrdJfaHR&ust=1727024012632000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKCBs_y_1IgDFQAAAAAdAAAAABAE"></img>
+            <h3>yellow Flowers</h3>
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
