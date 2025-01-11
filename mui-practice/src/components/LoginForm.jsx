af58e070ddd5f906b88eb0dd4b3bcf6a7d519a01
Sara-Gavi <sara.gavilan.castro@gmail.com>
2025-01-11 19:31:58 +0100

    Install Material UI, Roboto font, and icons

---
+++ mui-practice/src/components/LoginForm.jsx
@@ -0,0 +1,12 @@
+function LoginForm() {
+  return (
+    <div>
+      <h1>LOGIN</h1>
+      <input type="email" placeholder="Ingrese email" />
+      <input type="password" placeholder="Ingrese contraseña" />
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default LoginForm;
