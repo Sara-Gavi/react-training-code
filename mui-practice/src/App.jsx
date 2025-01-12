5190a4f5131aa108e98f3d41eeb17dab71f8c347
Sara-Gavi <sara.gavilan.castro@gmail.com>
2025-01-12 19:54:06 +0100

    Add Container component with maxWidth 'xs' to App

--- mui-practice/src/App.jsx
+++ mui-practice/src/App.jsx
@@ -1,10 +1,12 @@
-import { Button } from "@mui/material";
+import { Button, Container } from "@mui/material";
 
 function App() {
   return (
     <div>
-      <h1>App</h1>
-      <Button variant="contained">Hello World</Button>
+      <Container maxWidth="xs">
+        <h1>App</h1>
+        <Button variant="contained">Hello World</Button>
+      </Container>
     </div>
   );
 }
