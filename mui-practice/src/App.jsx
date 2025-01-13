b068451dcdeb199eb912b40300a58a2addf1af61
Sara-Gavi <sara.gavilan.castro@gmail.com>
2025-01-13 12:58:31 +0100

    Practice with Typography component: variants and alignment

--- mui-practice/src/App.jsx
+++ mui-practice/src/App.jsx
@@ -1,10 +1,17 @@
-import { Button, Container } from "@mui/material";
+import { Button, Container, Typography } from "@mui/material";
 
 function App() {
   return (
     <div>
       <Container sx={{ border: 2, boxShadow: 3, pb: 2 }}>
         <h1>App</h1>
+        <Typography variant="h1" component="h2">
+          Título 1
+        </Typography>
+        <Typography variant="h1">Título 2</Typography>
+        <Typography variant="body1" textAlign="center">
+          Título 3
+        </Typography>
         <Button variant="contained">Hello World</Button>
       </Container>
     </div>
