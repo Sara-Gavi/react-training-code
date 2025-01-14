02d7ba5f931aa4371ed0e9a3dc62d5c58e84f4fe
Sara-Gavi <sara.gavilan.castro@gmail.com>
2025-01-14 13:50:47 +0100

    Practice with Grid component: responsive layout with spacing and breakpoints

--- mui-practice/src/App.jsx
+++ mui-practice/src/App.jsx
@@ -1,41 +1,35 @@
-import { Box, Button, Container, Typography } from "@mui/material";
-import AddHomeWorkIcon from "@mui/icons-material/AddHomeWork";
-import BalanceIcon from "@mui/icons-material/Balance";
+import { Container, Grid } from "@mui/material";
 
 function App() {
   return (
     <div>
-      <Container sx={{ border: 2, boxShadow: 3, pb: 2 }}>
-        <h1>App</h1>
-        <Typography variant="h1" component="h2">
-          Título 1
-        </Typography>
-        <Typography variant="h1">Título 2</Typography>
-        <Typography variant="body1" textAlign="center">
-          Título 3
-        </Typography>
-        <Box
-          sx={{
-            border: 2,
-            p: 5,
-            borderColor: "peru",
-            bgcolor: "#111",
-            color: "white",
-          }}
-          component="span"
-        >
-          Que hermoso es MUI
-        </Box>
-        <Button
-          variant="contained"
-          color="error"
-          startIcon={<AddHomeWorkIcon />}
-        >
-          Hello World
-        </Button>
-        <Button variant="outlined" color="success" endIcon={<BalanceIcon />}>
-          Hello World
-        </Button>
+      <Container>
+        <Grid container spacing={5}>
+          <Grid item xs={12} sm={6} md={4}>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde quo
+              repudiandae explicabo quibusdam ducimus rerum veritatis, commodi,
+              recusandae expedita reprehenderit tenetur omnis perferendis
+              doloremque aspernatur magni cupiditate fugiat magnam alias!
+            </p>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde quo
+              repudiandae explicabo quibusdam ducimus rerum veritatis, commodi,
+              recusandae expedita reprehenderit tenetur omnis perferendis
+              doloremque aspernatur magni cupiditate fugiat magnam alias!
+            </p>
+          </Grid>
+          <Grid item xs={12} sm={12} md={4}>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde quo
+              repudiandae explicabo quibusdam ducimus rerum veritatis, commodi,
+              recusandae expedita reprehenderit tenetur omnis perferendis
+              doloremque aspernatur magni cupiditate fugiat magnam alias!
+            </p>
+          </Grid>
+        </Grid>
       </Container>
     </div>
   );
