72168e315024b47ff0a9778dcb1c7d9a4144c007
Sara-Gavi <sara.gavilan.castro@gmail.com>
2025-01-14 13:56:32 +0100

    Reset App component and create Product component for a new complete practice

--- mui-practice/src/App.jsx
+++ mui-practice/src/App.jsx
@@ -1,38 +1,5 @@
-import { Container, Grid } from "@mui/material";
-
 function App() {
-  return (
-    <div>
-      <Container>
-        <Grid container spacing={5}>
-          <Grid item xs={12} sm={6} md={4}>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde quo
-              repudiandae explicabo quibusdam ducimus rerum veritatis, commodi,
-              recusandae expedita reprehenderit tenetur omnis perferendis
-              doloremque aspernatur magni cupiditate fugiat magnam alias!
-            </p>
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde quo
-              repudiandae explicabo quibusdam ducimus rerum veritatis, commodi,
-              recusandae expedita reprehenderit tenetur omnis perferendis
-              doloremque aspernatur magni cupiditate fugiat magnam alias!
-            </p>
-          </Grid>
-          <Grid item xs={12} sm={12} md={4}>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde quo
-              repudiandae explicabo quibusdam ducimus rerum veritatis, commodi,
-              recusandae expedita reprehenderit tenetur omnis perferendis
-              doloremque aspernatur magni cupiditate fugiat magnam alias!
-            </p>
-          </Grid>
-        </Grid>
-      </Container>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
