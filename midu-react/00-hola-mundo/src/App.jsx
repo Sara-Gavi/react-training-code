fbab29310ae25fb8d681f0a95200761b82a9c58e
Sara-Gavi <sara.gavilan.castro@gmail.com>
2025-09-07 20:25:21 +0200

    refactor: extract TwitterFollowCard component

--- midu-react/00-hola-mundo/src/App.jsx
+++ midu-react/00-hola-mundo/src/App.jsx
@@ -1,22 +1,7 @@
 import "./App.css";
+import TwitterFollowCard from "./TwitterFollowCard";
+
 function App() {
-  return (
-    <article className="tw-followCard">
-      <header className="tw-followCard-header">
-        <img
-          className="tw-followCard-avatar"
-          alt="El avatar de Pikachu"
-          src="https://unavatar.io/pikachu"
-        />
-        <div className="tw-followCard-info">
-          <strong>Pikachu</strong>
-          <span className="tw-followCard-infoUserName">@Pikapika</span>
-        </div>
-      </header>
-      <aside>
-        <button className="tw-followCard-button">Seguir</button>
-      </aside>
-    </article>
-  );
+  return <TwitterFollowCard userName="pikachu" name="Pikachu" />;
 }
 export default App;
